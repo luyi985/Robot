e6119d1d323237ea965083b2c45614b08d5c6983
Yi Lu <luyi985@gmail.com>
2016-10-30 18:47:14 +1100

    that's all

--- Config.js
+++ Config.js
@@ -1,6 +1,6 @@
 module.exports={
 	tableSize 			: 			5,
-	dataLocation 		: 			"./data/data1.json",
+	dataLocation 		: 			"./data/data.json",
 	commandKeyWords 	: 			["PLACE","MOVE","LEFT","RIGHT","REPORT"],
 	wellcomeCopy		: 			"Wellcome to robot test,\r\nPlease input any of following command,\r\npress \'Enter\' to input next command\r\npress \'ctrl\+c\' to finish:)\r\n\r\n PLACE X\,Y\,F \r\n MOVE \r\n LEFT \r\n RIGHT \r\n REPORT \r\n",
 	afterCommandInput 	: 			"Please type \'npm run play\' to run the simulator"
